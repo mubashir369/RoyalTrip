93e31eadb20506fc05da2f6b2f43aa3637f30507
mubashir369 <muhammedmubashir3693@gmail.com>
2022-09-20 21:29:05 +0530

    add place in admin side

--- server/routers/admin.js
+++ server/routers/admin.js
@@ -3,4 +3,8 @@ const adminControl = require("../controllers/adminControl");
 const router = express.Router();
 
 router.post("/", adminControl.adminLogin);
+router.post("/addPlace", (req,res)=>{
+    console.log("place api called");
+    console.log(req.body);
+});
 module.exports = router;
